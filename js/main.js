548fd466a9bf7e83746cb9da1c81ea970473357c
Ivan Perun <peruni101@gmail.com>
2018-04-16 15:14:53 -0400

    content fix

--- js/main.js
+++ js/main.js
@@ -303,7 +303,7 @@ function populateInfoWindow(marker, street, city, infowindow) {
   // Check to make sure the infowindow is not already opened on this marker.
   if (infowindow.marker != marker) {
     infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + marker.position + '' + '</div>');
+    infowindow.setContent('<div>' + marker.title + marker.position + street + city + '</div>');
     infowindow.open(map, marker);
     // Make sure the marker property is cleared if the infowindow is closed.
     infowindow.addListener('closeclick', function() {
